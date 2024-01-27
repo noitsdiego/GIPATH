f85069d286ca4c77a2289df166d17fdcbd66149d
Diego Andres Riveros Lopez <diegoriveros06@gmail.com>
2024-01-27 09:54:44 -0500

    first commit

---
+++ src/main/java/com/userManage/githpath/GithpathApplication.java
@@ -0,0 +1,13 @@
+package com.userManage.githpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithpathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/userManage/githpath/GithpathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userManage.githpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithpathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
